12f93c8d53c144063d14ab660308581c885231f1
ElhanM <elhan.mahmutovic03@gmail.com>
2024-07-20 14:27:00 +0200

    Fix(app): jest config

--- app/jest.config.js
+++ app/jest.config.js
@@ -1,8 +1,4 @@
 module.exports = {
   preset: 'jest-expo',
   testEnvironment: 'jsdom',
-  transform: {
-    '^.+\\.[tj]sx?$': 'babel-jest',
-  },
-  transformIgnorePatterns: ['node_modules/(?!(toastify-react-native|@react-native|react-native)/)'],
 };
